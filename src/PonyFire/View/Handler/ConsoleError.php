c2c610e1b6cbb9513979e89ae751b4d37132829b
dhanuprys <dhanuprys@gmail.com>
2020-05-15 21:18:13 +0800

    Add error handler

---
+++ src/PonyFire/View/Handler/ConsoleError.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PonyFire\Views\Handler;
+
+use Whoops\Handler\Handler;
+
+class Console extends Handler
+{
+    public function handle()
+    {
+        
+    }
+}
---
+++ src/PonyFire/View/Handler/HttpProductionError.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PonyFire\Views\Handler;
+
+use Whoops\Handler\Handler;
+
+class HttpProductionError extends Handler
+{
+    public function handle()
+    {
+        
+    }
+}
